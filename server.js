36bf80107fd3e849561fdbe1791cf0b0b741b4f5
Adoniram Medeiros de Oliveira <adoniram.sqa@alterdata.matriz>
2019-04-24 11:24:32 -0300

    troca de host

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const port = 8080
-const ip = 'localhost'
+const ip = '0.0.0.0'
 
 const server = http.createServer((req, res) => {
   console.log('Recebendo uma request!')
